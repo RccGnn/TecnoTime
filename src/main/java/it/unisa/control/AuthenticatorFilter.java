33da435ff48cab5c9bffb66a48fb6e1509f43a62
StefanoGit50 <s.santoro41@studenti.unisa.it>
2025-06-26 02:16:20 +0200

    login page login logout

---
+++ src/main/java/it/unisa/control/AuthenticatorFilter.java
@@ -0,0 +1,62 @@
+package it.unisa.control;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.FilterConfig;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.net.http.HttpRequest;
+
+/**
+ * Servlet Filter implementation class AuthenticatorFilter
+ */
+//@WebFilter("/AuthenticatorFilter")
+public class AuthenticatorFilter extends HttpFilter implements Filter {
+       
+    /**
+     * @see HttpFilter#HttpFilter()
+     */
+    public AuthenticatorFilter() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see Filter#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
+	 */
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		
+		HttpServletRequest req = (HttpServletRequest) request;
+		HttpServletResponse res = (HttpServletResponse) response;
+		HttpSession session = req.getSession(false);
+		if (session == null || session.getAttribute("user") == null) {
+		    res.sendRedirect(req.getContextPath()+"/LoginPage.jsp");		
+		    return;
+		}
+		// pass the request along the filter chain
+		chain.doFilter(request, response);
+	}
+
+	/**
+	 * @see Filter#init(FilterConfig)
+	 */
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
---
+++ src/main/java/it/unisa/control/LoginPage.java
@@ -0,0 +1,86 @@
+package it.unisa.control;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import it.unisa.model.DAO.AccountDao;
+import it.unisa.model.DAO.BeanDaoInterface;
+import it.unisa.model.beans.AccountBean;
+
+/**
+ * Servlet implementation class LoginPage
+ */
+@WebServlet("/LoginPage")
+public class LoginPage extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginPage() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		
+		String username= request.getParameter("username");
+        String pwd = request.getParameter("password");
+        
+        username.trim();
+        pwd.trim();
+        BeanDaoInterface<AccountBean> dao= new AccountDao();
+        AccountBean account = new AccountBean();
+        
+        try {
+        	 account = dao.doRetrieveByKey(username);
+        	 if(account !=null && username.equals(account.getUsername())) {
+        		 if(PasswordUtils.checkPasswordHashed(pwd, account.gethashedPassword())==true) {
+        			 request.setAttribute("flag","passok");
+        		 }else {
+        			 request.setAttribute("error", "username o password errati"); 
+        	         RequestDispatcher dispatcher = request.getRequestDispatcher("/LoginPage.jsp");
+        	         dispatcher.forward(request, response);      
+        	         return;
+        		 }
+             }
+        }catch(SQLException e) {
+        	request.setAttribute("serverError", "Login Fallito. Riprova"); //eventuale pagina errore
+        }
+       
+        //creazione sessione utente
+        HttpSession session = request.getSession();
+        session.setAttribute("user", account);
+        
+        
+        // Redirect a pagina protetta
+        response.sendRedirect(request.getContextPath() + "/index.jsp");
+       
+       
+     
+
+        
+	}
+
+}
