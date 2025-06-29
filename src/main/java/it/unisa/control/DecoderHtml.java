dd443fba68cf6d7aa8b67355125a673b5a7809ae
RccGnn <g.riccardi4@studenti.unisa.it>
2025-06-29 19:11:05 +0200

    Spostati alcuni metodi da DecoderHtml a DaoUtils

--- src/main/java/it/unisa/control/DecoderHtml.java
+++ src/main/java/it/unisa/control/DecoderHtml.java
@@ -11,29 +11,5 @@ public class DecoderHtml {
 	public static String encodeHtml(String input) {
 		if(input==null) return null;
 		return  input.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;").replace("\"", "&quot;").replace("'","&#39").replace(" ","&nbsp;");
-	} 
-	
-	/**
-	 * Metodo di utilità per far visualizzare l'immagine contenuta nel link di dropbox piuttosto che la pagina html
-	 * @param 	url {@code String} - url da convertire
-	 * @return	{@code String} urlDecoded dove il parametro "dl=0" è sostituito con "raw=1"
-	 */
-	public static String dropboxUrlDecoder(String url) {
-		if(url == null || !url.contains("www.dropbox.com"))
-			return null;
-		else
-			return url.replace("dl=0", "raw=1");
-	}
-
-	/**
-	 * Metodo di utilità per far visualizzare la pagina html piuttosto che l'immagine contenuta nel link di dropbox
-	 * @param 	url {@code String} - url da convertire
-	 * @return	{@code String} urlDecoded dove il parametro "raw=1" è sostituito con "dl=0"
-	 */
-	public static String dropboxUrlEncoder(String url) {
-		if(url == null || !url.contains("www.dropbox.com"))
-			return null;
-		else
-			return url.replace("raw=1", "dl=0");
 	}
 }
--- src/main/java/it/unisa/model/DAO/DaoUtils.java
+++ src/main/java/it/unisa/model/DAO/DaoUtils.java
@@ -52,4 +52,28 @@ public class DaoUtils {
 		
 		return ruolo;
 	}
+	
+	/**
+	 * Metodo di utilità per far visualizzare l'immagine contenuta nel link di dropbox piuttosto che la pagina html
+	 * @param 	url {@code String} - url da convertire
+	 * @return	{@code String} urlDecoded dove il parametro "dl=0" è sostituito con "raw=1"
+	 */
+	public static String dropboxUrlDecoder(String url) {
+		if(url == null || !url.contains("www.dropbox.com"))
+			return null;
+		else
+			return url.replace("dl=0", "raw=1");
+	}
+
+	/**
+	 * Metodo di utilità per far visualizzare la pagina html piuttosto che l'immagine contenuta nel link di dropbox
+	 * @param 	url {@code String} - url da convertire
+	 * @return	{@code String} urlDecoded dove il parametro "raw=1" è sostituito con "dl=0"
+	 */
+	public static String dropboxUrlEncoder(String url) {
+		if(url == null || !url.contains("www.dropbox.com"))
+			return null;
+		else
+			return url.replace("raw=1", "dl=0");
+	}
 }
